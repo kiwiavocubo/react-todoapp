367735dd16b5d84b940d8b6782ee0433e81a0e3c
kiwiAvocubo <vitahu1005@gmail.com>
2019-01-02 10:37:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Note from './components/Note';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      noteText: '',
+      notes:[]
+    }
+  }
+
+  updateNotetext(noteText){
+    this.setState({noteText:noteText.target.value})
+  }
+
+  addNote(){
+    if (this.state.noteText===''){return}
+
+    let notesArr =this.state.notes;
+    notesArr.push(this.state.noteText);
+    this.setState({ noteText:''});
+    this.textInput.focus();
+}
+  handleKeyPress =(event) =>{
+    if (event.key==='Enter')
+    {
+      let notesArr =this.state.notes;
+      notesArr.push(this.state.noteText);
+      this.setState({ noteText:''});
+    }
+  }
+
+  deleteNote(index) {
+    let notesArr =this.state.notes;
+    notesArr.splice(index,1);
+    this.setState({ notes:notesArr})
+  }
+
   render() {
+
+    let notes = this.state.notes.map((val,key)=>{
+      return <Note key={key} text={val}
+      deleteMethod={()=>this.deleteNote(key)} />
+    }) 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="container">
+
+       <div className="header">REACT TODO APPLICATION</div>
+       {notes}
+       <div className="btn" onClick={this.addNote.bind(this)}>+</div>
+       <input type="text" 
+          ref={((input) => {this.textInput=input})}
+            className="textInput"
+            value={ this.state.noteText}
+            onChange={noteText=>this.updateNotetext(noteText)}
+            onKeyPress={this.handleKeyPress.bind(this)}
+          />
+        </div>
     );
   }
 }
